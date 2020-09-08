56a56b5d552b3ee803e2365589c968452574aebd
SeReSs52 <67641764+SeReSs52@users.noreply.github.com>
2020-09-08 15:40:47 +0300

    my commit

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ from django.urls import reverse
 from django.utils import dateformat
 
 
+
+
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager, self).get_queryset()\
@@ -35,3 +38,17 @@ class Post(models.Model):
         ordering =('-publish',)
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    def __str__(self):
+        return f'Коментарий {self.name} on {self.post} '
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from .models import Post, Comment
+from .forms import CommentForm
+
+
+
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 3)
@@ -16,5 +20,23 @@ def post_list(request):
 
 def post_detail(request, post):
     post = get_object_or_404(Post, slug = post)
+
+    #Список активных комментариев этих постов
+    comments = post.comments.filter(active = True)
+
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data = request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
     return render(request, 'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'new_comment': new_comment,
+                   'comment_form': comment_form})
