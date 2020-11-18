79836a034fce68a599f698e89c97aee903e33a8d
SeReSs52 <67641764+SeReSs52@users.noreply.github.com>
2020-11-18 15:06:06 +0300

    asda

--- blog/models.py
+++ blog/models.py
@@ -46,7 +46,7 @@ class Comment(models.Model):
     body = models.TextField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
+    active = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('created',)
